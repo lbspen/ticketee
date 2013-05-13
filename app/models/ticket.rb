18d27b842f21740fff4726f6d23cf2bf8addf8da
lbs <lbspen@gmail.com>
2013-05-13 16:20:50 -0700

    Can view tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,7 @@ class Ticket < ActiveRecord::Base
   belongs_to :project
   attr_accessible :description, :title
 
-  validates :description, :presence => true
+  validates :description, :presence => true,
+    :length => {:minimum => 10}
   validates :title, :presence => true
 end
