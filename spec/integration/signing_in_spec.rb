94836c511790006cd76f3627eca8d9ed88c354d1
lbs <lbspen@gmail.com>
2013-05-14 23:18:06 -0700

    Associates users with tickets

--- spec/integration/signing_in_spec.rb
+++ spec/integration/signing_in_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 feature 'Signing in' do
   before do
-    @user = Factory(:user)
+    Factory(:user, :email => "ticketee@example.com")
   end
 
   scenario 'Signing in via confirmation' do
-    open_email @user.email, :with_subject => /Confirmation/
+    open_email "ticketee@example.com", :with_subject => /Confirmation/
     click_first_link_in_email
     page.should have_content("Your account was successfully confirmed")
-    page.should have_content("Signed in as #{@user.email}")
+    page.should have_content("Signed in as ticketee@example.com")
   end
 
   scenario "Signing in via form" do
-    @user.confirm!
+    User.find_by_email("ticketee@example.com").confirm!
     visit '/'
     click_link "Sign in"
-    fill_in "Email", :with => @user.email
+    fill_in "Email", :with => "ticketee@example.com"
     fill_in "Password", :with => "password"
     click_button "Sign in"
     page.should have_content("Signed in successfully.")
