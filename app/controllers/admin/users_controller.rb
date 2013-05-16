59b785ffe42f8f3bdd4e5ca74635fc0e60176c24
lbs <lbspen@gmail.com>
2013-05-16 00:31:18 -0700

    admin can delete users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -33,7 +33,7 @@ class Admin::UsersController < Admin::BaseController
       params[:user].delete(:password)
       params[:user].delete(:password_confirmation)
     end
-    
+
     if @user.update_attributes(params[:user], :as => :admin)
       flash[:notice] = "User has been updated."
       redirect_to admin_users_path
@@ -41,6 +41,17 @@ class Admin::UsersController < Admin::BaseController
       flash[:alert] = "User has not been updated."
       render :action => "edit"
     end
+  end
+
+  def destroy
+    if @user == current_user
+      flash[:alert] = "You cannot delete yourself!"
+    else
+      @user.destroy
+      flash[:notice] = "User has been deleted."
+    end
+
+    redirect_to admin_users_path
   end
 
   private
