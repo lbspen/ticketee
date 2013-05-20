500e606803ccb989c6b17febd513c481533df3db
lbs <lbspen@gmail.com>
2013-05-20 00:54:36 -0700

    hid links based on permissions

--- spec/integration/deleting_tickets_spec.rb
+++ spec/integration/deleting_tickets_spec.rb
@@ -3,24 +3,48 @@ require 'spec_helper'
 feature "Deleting tickets" do 
   let!(:project) { Factory(:project) }
   let!(:user) { Factory(:confirmed_user) }
+  let!(:admin) { Factory(:admin_user) }
   let!(:ticket) do
     ticket = Factory(:ticket, :project => project) 
     ticket.update_attribute(:user, user)
     ticket
   end
 
-  before do
-    define_permission!(user, "view", project)
-    define_permission!(user, "delete tickets", project)
-    sign_in_as!(user)
-    visit '/'
-    click_link project.name
-    click_link ticket.title
+  context "standard users" do
+    before do
+      define_permission!(user, "view", project)
+      define_permission!(user, "delete tickets", project)
+      sign_in_as!(user)
+      visit '/'
+      click_link project.name
+      click_link ticket.title
+    end
+
+    scenario "Deleting a ticket" do 
+      click_link "Delete Ticket"
+      page.should have_content("Ticket has been deleted.")
+      page.current_url.should == project_url(project)
+    end
+
+    scenario "New ticket link is shown to a user with permission" do
+      define_permission!(user, "view", project)
+      define_permission!(user, "create tickets", project)
+      visit project_path(project)
+      assert_link_for "New Ticket"
+    end
+
+    scenario "New ticket link is hidden from a user without permission" do
+      define_permission!(user, "view", project)
+      visit project_path(project)
+      assert_no_link_for "New Ticket"
+    end
   end
 
-  scenario "Deleting a ticket" do 
-    click_link "Delete Ticket"
-    page.should have_content("Ticket has been deleted.")
-    page.current_url.should == project_url(project)
+  context "admin users" do
+    before do
+      sign_in_as!(admin)
+      visit project_path(project)
+      assert_no_link_for "New Ticket"
+    end
   end
 end
--- spec/integration/hidden_links_spec.rb
+++ spec/integration/hidden_links_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 feature "Hidden Links -" do
-  let(:user) { Factory(:confirmed_user) }
-  let(:admin) { Factory(:admin_user) }
-  let(:project) { Factory(:project) } 
+  let!(:user) { Factory(:confirmed_user) }
+  let!(:admin) { Factory(:admin_user) }
+  let!(:project) { Factory(:project) } 
+  let!(:ticket) { Factory(:ticket, :project => project, :user => user) }
 
   context "anonymous users" do
     scenario "cannot see the New Project link" do
@@ -38,6 +39,36 @@ feature "Hidden Links -" do
       visit project_path(project)
       assert_no_link_for "Delete Project"
     end
+
+    scenario "Edit ticket link is shown to a user with permission" do
+      define_permission!(user, "view", project)
+      define_permission!(user, "edit tickets", project)
+      visit project_path(project)
+      click_link ticket.title
+      assert_link_for "Edit Ticket"
+    end
+
+    scenario "Edit ticket link is hidden from a user without permission" do
+      define_permission!(user, "view", project)
+      visit project_path(project)
+      click_link ticket.title
+      assert_no_link_for "Edit Ticket"
+    end
+
+    scenario "Delete ticket link is shown to a user with permission" do
+      define_permission!(user, "view", project)
+      define_permission!(user, "delete tickets", project)
+      visit project_path(project)
+      click_link ticket.title
+      assert_link_for "Delete Ticket"
+    end
+
+    scenario "Delete ticket link is hidden from users without permission" do
+      define_permission!(user, "view", project)
+      visit project_path(project)
+      click_link ticket.title
+      assert_no_link_for "Delete Ticket"
+    end
   end
 
   context "admin users" do
@@ -55,6 +86,18 @@ feature "Hidden Links -" do
     scenario "can see the Delete Project link" do
       visit project_path(project)
       assert_link_for "Delete Project"
+    end
+
+    scenario "Edit ticket link is shown to admins" do
+      visit project_path(project)
+      click_link ticket.title
+      assert_link_for "Edit Ticket"
+    end
+
+    scenario "Delete ticket link is shown to admins" do
+      visit project_path(project)
+      click_link ticket.title
+      assert_link_for "Delete Ticket"
     end
   end
 end
