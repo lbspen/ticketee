b37ae7b819baa37f37b7353c37bb10bd9e8581a4
lbs <lbspen@gmail.com>
2013-05-19 19:29:17 -0700

    Don't list projects a user doesn't have permission to see

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,15 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :name
   has_many :tickets, :dependent => :delete_all
-  validates :name, :presence => true, :uniqueness => true
-
   has_many :permissions, :as => :thing
+  validates :name, :presence => true, :uniqueness => true
+  
   def self.viewable_by(user)
     joins(:permissions).where(:permissions => { :action => "view",
                                                 :user_id => user.id })
   end
 
+  def self.for(user)
+    user.admin? ? Project : Project.viewable_by(user)
+  end
 end
