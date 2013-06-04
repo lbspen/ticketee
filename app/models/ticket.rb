7205769bd3559f1d957aadb52b084a02068bfb25
lbs <lbspen@gmail.com>
2013-06-03 21:21:21 -0700

    Create Asset model to handle upload of 3 files

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,12 +1,12 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
-  
-  attr_accessible :description, :title, :asset
+  has_many :assets
+
+  accepts_nested_attributes_for :assets
+  attr_accessible :description, :title, :assets_attributes
 
   validates :description, :presence => true,
     :length => {:minimum => 10}
   validates :title, :presence => true
-
-  has_attached_file :asset
 end
