f7c4ea6dba8d751f5a499fe95a193265d74cde7d
lbs <lbspen@gmail.com>
2013-06-03 23:17:03 -0700

    Don't serve files to those without permission

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,6 @@
 class Asset < ActiveRecord::Base
-  # attr_accessible :title, :body
+  belongs_to :ticket
+  has_attached_file :asset, :path => (Rails.root + "files/:id").to_s,
+    :url => "/files/:id"
   attr_accessible :asset
-  has_attached_file :asset
 end
